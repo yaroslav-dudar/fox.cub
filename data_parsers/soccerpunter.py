6e5f570a22b0b96574edc79420f59263615bf08f
Yaroslav Dudar <flayingfog@gmail.com>
2019-05-25 09:05:55 +0300

    upgrade football dataset preparation

--- data_parsers/soccerpunter.py
+++ data_parsers/soccerpunter.py
@@ -19,46 +19,58 @@ class SoccerPunterSpider(scrapy.Spider):
     name = 'soccerpunter'
 
     championship = ["/England/Championship-{0}-{1}".format(year, year+1)
-        for year in range(1993, 2018)]
+        for year in range(1993, 2019)]
 
     epl = ["/England/Premier-League-{0}-{1}".format(year, year+1)
-        for year in range(1965, 2018)]
+        for year in range(1965, 2019)]
 
     bundesliga = ["/Germany/Bundesliga-{0}-{1}".format(year, year+1)
-        for year in range(1963, 2018)]
+        for year in range(1963, 2019)]
 
     bundesliga2 = ["/Germany/2.-Bundesliga-{0}-{1}".format(year, year+1)
-        for year in range(1993, 2018)]
+        for year in range(1993, 2019)]
 
     bundesliga3 = ["/Germany/3.-Liga-{0}-{1}".format(year, year+1)
-        for year in range(2008, 2018)]
+        for year in range(2008, 2019)]
 
     efl_league1 = ["/England/League-One-{0}-{1}".format(year, year+1)
-        for year in range(1993, 2018)]
+        for year in range(1993, 2019)]
 
     laliga = ["/Spain/La-Liga-{0}-{1}".format(year, year+1)
-        for year in range(1993, 2018)]
+        for year in range(1993, 2019)]
 
     segunda = ["/Spain/Segunda-División-{0}-{1}".format(year, year+1)
-        for year in range(1996, 2018)]
+        for year in range(1996, 2019)]
 
     serie_a = ["/Italy/Serie-A-{0}-{1}".format(year, year+1)
-        for year in range(1993, 2018)]
+        for year in range(1993, 2019)]
 
     serie_b = ["/Italy/Serie-B-{0}-{1}".format(year, year+1)
-        for year in range(1993, 2018)]
+        for year in range(1993, 2019)]
+
+    serie_c = ["/Italy/Serie-C-{0}-{1}".format(year, year+1)
+        for year in range(1999, 2019)]
 
     france_ligue1 = ["/France/Ligue-1-{0}-{1}".format(year, year+1)
-        for year in range(1994, 2018)]
+        for year in range(1994, 2019)]
+
+    france_ligue2 = ["/France/Ligue-2-{0}-{1}".format(year, year+1)
+        for year in range(1994, 2019)]
 
     belgium_div1 = ["/Belgium/First-Division-A-{0}-{1}".format(year, year+1)
         for year in range(1995, 2018)]
 
     eradivisie = ["/Netherlands/Eredivisie-{0}-{1}".format(year, year+1)
-        for year in range(1993, 2018)]
+        for year in range(1993, 2019)]
+
+    eereste_divicie = ["/Netherlands/Eerste-Divisie-{0}-{1}".format(year, year+1)
+        for year in range(1996, 2019)]
 
     swiss_super_league = ["/Switzerland/Super-League-{0}-{1}".format(year, year+1)
-        for year in range(1994, 2018)]
+        for year in range(1994, 2019)]
+
+    swiss_chalange_league = ["/Switzerland/Challenge-League-{0}-{1}".format(year, year+1)
+        for year in range(1999, 2019)]
 
     slovakia_super_liga = ["/Slovakia/Super-Liga-{0}-{1}".format(year, year+1)
         for year in range(2002, 2018)]
@@ -73,12 +85,18 @@ class SoccerPunterSpider(scrapy.Spider):
         for year in range(1994, 2018)]
 
     portugal_liga = ["/Portugal/Primeira-Liga-{0}-{1}".format(year, year+1)
-        for year in range(1994, 2018)]
+        for year in range(1994, 2019)]
 
     mls = ["/USA/MLS-{0}".format(year)
         for year in range(2000, 2018)]
 
-    ec_qualification = [
+    austria_bundesliga = ["/Austria/Bundesliga-{0}-{1}".format(year, year+1)
+        for year in range(1993, 2019)]
+
+    scotland_premiership = ["/Scotland/Premiership-{0}-{1}".format(year, year+1)
+        for year in range(1994, 2019)]
+
+    eu_qualification = [
         "/Europe/EC-Qualification-2012-Poland-Ukraine/results",
         "/Europe/EC-Qualification-2016-France",
         "/Europe/EC-Qualification-2008-Austria-Switzerland",
@@ -98,6 +116,93 @@ class SoccerPunterSpider(scrapy.Spider):
         "/Europe/WC-Qualification-Europe-1994-USA"
     ]
 
+    eu_championship = [
+        "/Europe/European-Championship-2016-France",
+        "/Europe/European-Championship-2012-Poland-Ukraine",
+        "/Europe/European-Championship-2008-Austria-Switzerland",
+        "/Europe/European-Championship-2004-Portugal",
+        "/Europe/European-Championship-2000-Netherlands-Belgium",
+        "/Europe/European-Championship-1996-England",
+        "/Europe/European-Championship-1992-Sweden",
+        "/Europe/European-Championship-1988-Germany",
+        "/Europe/European-Championship-1984-France"
+    ]
+
+    sa_qualification = [
+        "/South-America/WC-Qualification-South-America-2018-Russia",
+        "/South-America/WC-Qualification-South-America-2014-Brazil",
+        "/South-America/WC-Qualification-South-America-2010-South-Africa",
+        "/South-America/WC-Qualification-South-America-2006-Germany",
+        "/South-America/WC-Qualification-South-America-2002-Korea-Rep-Japan"
+    ]
+
+    copa_america = [
+        "/South-America/Copa-America-2016-USA",
+        "/South-America/Copa-America-2015-Chile",
+        "/South-America/Copa-America-2011-Argentina",
+        "/South-America/Copa-America-2007-Venezuela",
+        "/South-America/Copa-America-2004-Peru",
+        "/South-America/Copa-America-2001-Colombia",
+        "/South-America/Copa-America-1999-Paraguay",
+        "/South-America/Copa-America-1997-Bolivia",
+        "/South-America/Copa-America-1995-Uruguay"
+    ]
+
+    africa_qualification = [
+        "/Africa/WC-Qualification-Africa-2018-Russia",
+        "/Africa/WC-Qualification-Africa-2014-Brazil",
+        "/Africa/WC-Qualification-Africa-2010-South-Africa",
+        "/Africa/WC-Qualification-Africa-2006-Germany",
+        "/Africa/WC-Qualification-Africa-2002-Korea-Rep-Japan"
+    ]
+
+    africa_cup = [
+        "/Africa/Africa-Cup-of-Nations-2017-Gabon",
+        "/Africa/Africa-Cup-of-Nations-2015-Equatorial-Guinea",
+        "/Africa/Africa-Cup-of-Nations-2013-South-Africa",
+        "/Africa/Africa-Cup-of-Nations-2012-Equatorial-Guinea-Gabon",
+        "/Africa/Africa-Cup-of-Nations-2010-Angola",
+        "/Africa/Africa-Cup-of-Nations-2008-Ghana",
+        "/Africa/Africa-Cup-of-Nations-2006-Egypt",
+        "/Africa/Africa-Cup-of-Nations-2004-Tunisia",
+        "/Africa/Africa-Cup-of-Nations-2002-Mali",
+        "/Africa/Africa-Cup-of-Nations-2000-Ghana-Nigeria",
+        "/Africa/Africa-Cup-of-Nations-1998-Burkina-Faso"
+    ]
+
+    asia_qualification = [
+        "/Asia/Asian-Cup-Qualification-2019-UAE",
+        "/Asia/Asian-Cup-Qualification-2015-Australia",
+        "/Asia/Asian-Cup-Qualification-2011-Qatar",
+        "/Asia/Asian-Cup-Qualification-2007-Indonesia---Malaysia---Thailand---Vietnam",
+        "/Asia/Asian-Cup-Qualification-2004-China"
+    ]
+
+    asia_cup = [
+        "/Asia/AFC-Asian-Cup-2019-UAE",
+        "/Asia/AFC-Asian-Cup-2015-Australia",
+        "/Asia/AFC-Asian-Cup-2011-Qatar",
+        "/Asia/AFC-Asian-Cup-2007-Indonesia---Malaysia---Thailand---Vietnam",
+        "/Asia/AFC-Asian-Cup-2004-China"
+    ]
+
+    gold_cup = [
+        "/N-C-America/CONCACAF-Gold-Cup-2017-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-2015-USA-Canada",
+        "/N-C-America/CONCACAF-Gold-Cup-2013-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-2011-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-2009-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-2007-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-2005-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-2003-USA-Mexico",
+        "/N-C-America/CONCACAF-Gold-Cup-2002-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-2000-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-1998-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-1996-USA",
+        "/N-C-America/CONCACAF-Gold-Cup-1993-USA-Mexico",
+        "/N-C-America/CONCACAF-Gold-Cup-1991-USA"
+    ]
+
     fa_cup = ["/England/FA-Cup-{0}-{1}".format(year, year+1)
         for year in range(2004, 2019)]
 
@@ -107,7 +212,34 @@ class SoccerPunterSpider(scrapy.Spider):
     copa_del_rey = ["/Spain/Copa-del-Rey-{0}-{1}".format(year, year+1)
         for year in range(2004, 2019)]
 
-    tournaments = copa_del_rey
+    scotland_fa_cup = ["/Scotland/FA-Cup-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    coupe_de_france = ["/France/Coupe-de-France-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    copa_italia = ["/Italy/Coppa-Italia-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    knvb_baker = ["/Netherlands/KNVB-Beker-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    taga_de_portugal = ["/Portugal/Taça-de-Portugal-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    austria_cup = ["/Austria/Cup-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    swiss_pokal = ["/Switzerland/Schweizer-Pokal-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    copa_de_ligue = ["/France/Coupe-de-la-Ligue-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    league_cup = ["/England/League-Cup-{0}-{1}".format(year, year+1)
+        for year in range(2004, 2019)]
+
+    tournaments = championship
     base_url = "https://www.soccerpunter.com/soccer-statistics"
 
     h_timings = defaultdict(list)
--- stats_toolkit/generate_dataset.py
+++ stats_toolkit/generate_dataset.py
@@ -31,12 +31,33 @@ def game_totals(home_goals, away_goals):
 def individual_totals(goals):
     return goals
 
-def filter_by_date(max_day, max_month):
+def filter_by_month(min_month, max_month):
     def func(game):
         game_date = datetime.strptime(game['Date'], '%d/%m/%Y')
-        return game_date.month <= max_month and game_date.day <= max_day
+        return game_date.month <= max_month and game_date.month >= min_month
 
     return func
+
+def get_team_stats(stats_data_list, team):
+    for idx, stats in enumerate(stats_data_list[:1]):
+        try:
+            team_scores = get_team_scores(stats, team)
+        except Exception as e:
+            print(e)
+            return None
+
+        if team_scores['scored_xg']:
+            try:
+                season_totals = get_totals(stats)
+            except Exception as e:
+                raise SystemExit(e)
+
+            return [
+                season_totals['avgScoredHome'] + season_totals['avgScoredAway'],
+                team_scores['scored_xg'], team_scores['conceded_xg'], idx
+            ]
+
+    return None
 
 def prepare_dataset(input_dataset, stats_dataset, year_from, year_to):
     """Generate dataset for Fox.Cub statistical model
@@ -56,38 +77,31 @@ def prepare_dataset(input_dataset, stats_dataset, year_from, year_to):
         scored, conceded = {}, {}
 
         input_games = filter_by_season(input_dataset, str(season))
-        stats_data = filter_by_season(stats_dataset, str(season))
+        stats_data = [filter_by_season(stats, str(season)) for stats in stats_dataset]
 
         if not input_games: continue
-
-        try:
-            season_totals = get_totals(stats_data)
-        except Exception as e:
-            print(e)
-            continue
-
-        avg_goals_per_game = season_totals['avgScoredHome'] +\
-            season_totals['avgScoredAway']
 
         games_in_season = len(get_season_teams(input_games))*2 - 2
 
         sorted_games = sorted(input_games, key=lambda g: datetime.strptime(g['Date'], '%d/%m/%Y'))
+        #sorted_games = list(filter(filter_by_month(1, 7) , sorted_games))
 
         for i, g in enumerate(sorted_games):
 
-            home_team = get_team_scores(stats_data, g['HomeTeam'])
-            away_team = get_team_scores(stats_data, g['AwayTeam'])
+            home_team = get_team_stats(stats_data, g['HomeTeam'])
+            away_team = get_team_stats(stats_data, g['AwayTeam'])
 
-            if not home_team['conceded_xg'] or not away_team['scored_xg']: continue
+            if not home_team or not away_team: continue
 
             dataset.append([
-                game_scoreline(int(g['FTHG']), int(g['FTAG']))] +\
+                game_totals(int(g['FTHG']), int(g['FTAG']))] +\
                 [
-                    avg_goals_per_game,
-                    sum(home_team['scored_xg'])/len(home_team['scored_xg']),
-                    sum(home_team['conceded_xg'])/len(home_team['conceded_xg']),
-                    sum(away_team['scored_xg'])/len(away_team['scored_xg']),
-                    sum(away_team['conceded_xg'])/len(away_team['conceded_xg'])
+                    home_team[0], #away_team[0],
+                    #home_team[3], away_team[3],
+                    sum(home_team[1])/len(home_team[1]),
+                    sum(home_team[2])/len(home_team[2]),
+                    sum(away_team[1])/len(away_team[1]),
+                    sum(away_team[2])/len(away_team[2])
                 ]
             )
     return dataset
@@ -111,11 +125,18 @@ mls_btts = ['mls_regular.json', 'bundesliga.json']
 bundesliga = ['bundesliga.json', 'bundesliga2.json', 'swiss.json', 'epl.json', 'mls.json']
 
 club_playoffs = [
-    {'input': '../fa_cup.json', 'stats': 'efl.json'},
-    {'input': '../fa_cup.json', 'stats': 'epl.json'},
-    {'input': '../dfb_pokal.json', 'stats': 'bundesliga.json'},
-    {'input': '../dfb_pokal.json', 'stats': 'bundesliga2.json'},
-    {'input': '../copa_del_rey.json', 'stats': 'laliga.json'},
+    {'input': './nation_cups/scotland_fa_cup.json', 'stats': ['scotland_premiership.json']},
+    {'input': './nation_cups/fa_cup.json', 'stats': ['epl.json', 'championship.json', 'efl_league1.json']},
+    {'input': './nation_cups/league_cup.json', 'stats': ['epl.json', 'championship.json', 'efl_league1.json']},
+    {'input': './nation_cups/dfb_pokal.json', 'stats': ['bundesliga.json', 'bundesliga2.json', 'bundesliga3.json']},
+    {'input': './nation_cups/coupe_de_france.json', 'stats': ['france_ligue1.json', 'france_ligue2.json']},
+    {'input': './nation_cups/copa_de_ligue.json', 'stats': ['france_ligue1.json', 'france_ligue2.json']},
+    {'input': './nation_cups/copa_del_rey.json', 'stats': ['laliga.json', 'segunda.json']},
+    {'input': './nation_cups/copa_italia.json', 'stats': ['serie_a.json', 'serie_b.json']},
+    #{'input': './nation_cups/knvb_baker.json', 'stats': ['eradivisie.json', 'eereste_divicie.json']},
+    {'input': './nation_cups/taga_de_portugal.json', 'stats': ['portugal_liga.json']},
+    #{'input': './nation_cups/swiss_pokal.json', 'stats': ['swiss_super_league.json', 'swiss_chalange_league.json']},
+    #{'input': './nation_cups/austria_cup.json', 'stats': ['austria_bundesliga.json']},
 ]
 
 if __name__ == '__main__':
@@ -128,9 +149,9 @@ if __name__ == '__main__':
 
     for d in club_playoffs:
         input_data = readfile("{0}/{1}".format(data_folder, d['input']))
-        stats_data = readfile("{0}/{1}".format(data_folder, d['stats']))
+        stats_data = [readfile("{0}/{1}".format(data_folder, s)) for s in d['stats']]
 
-        output_dataset.extend(prepare_dataset(input_data, stats_data, 2000, 2018))
+        output_dataset.extend(prepare_dataset(input_data, stats_data, 2000, 2019))
 
     output_file = 'output.csv'
 
