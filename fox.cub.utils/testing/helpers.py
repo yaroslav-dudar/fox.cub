a8a36d8ad499d693904a28422ec24c8101186af1
Yaroslav Dudar <flayingfog@gmail.com>
2019-07-12 15:11:17 +0300

    move processing results logic away from slave object

--- fox.cub.utils/testing/helpers.py
+++ fox.cub.utils/testing/helpers.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABCMeta, abstractmethod
+import functools
+
 from utils import *
 
 class TestSessionResult():
 
-    def __init__(self):
+    def __init__(self, teams_1: list = None, teams_2: list = None):
         self.totals_2_5, self.totals_3_5 = [], []
         self.actual_results_team1, self.actual_results_team2 = [], []
         self.model_results = []
         self.scored_1, self.conceded_1 = [], []
         self.scored_2, self.conceded_2 = [], []
         self.btts = []
+
+        self.teams_1, self.teams_2 = teams_1, teams_2
 
     def __iadd__(self, other_session):
         self.totals_2_5 += other_session.totals_2_5
@@ -38,8 +42,50 @@ class TestSessionResult():
         self.scored_2, self.conceded_2 = [], []
         self.btts = []
 
+    def set_scoring_results(self, results):
+        scoring_table = get_season_table(results, metric='scored')
+        cons_table = get_season_table(results, metric='conceded')
+
+        for t in self.teams_1:
+            number_of_games = get_team_games(results, t)
+            self.scored_1.append(scoring_table[t] / number_of_games)
+            self.conceded_1.append(cons_table[t] / number_of_games)
+
+        for t in self.teams_2:
+            number_of_games = get_team_games(results, t)
+            self.scored_2.append(scoring_table[t] / number_of_games)
+            self.conceded_2.append(cons_table[t] / number_of_games)
+
+    def set_actual_results(self, game):
+        if game['HomeTeam'] in self.teams_1:
+            self.actual_results_team1.\
+                append(float(game['FTHG']) - float(game['FTAG']))
+            self.actual_results_team2.\
+                append(float(game['FTAG']) - float(game['FTHG']))
+        else:
+            self.actual_results_team2.\
+                append(float(game['FTHG']) - float(game['FTAG']))
+            self.actual_results_team1.\
+                append(float(game['FTAG']) - float(game['FTHG']))
+
+        self.btts.append(float(game['FTHG']) > 0 and float(game['FTAG']) > 0)
+
+    def set_model_results(self, prediction):
+        if prediction['HomeTeam'] in self.teams_1:
+            prediction['Team1'] = "Home"
+            prediction['Team2'] = "Away"
+        else:
+            prediction['Team1'] = "Away"
+            prediction['Team2'] = "Home"
+
+        self.model_results.append(prediction)
+
 
 class BasePattern(metaclass=ABCMeta):
+
+    def __init__(self, dataset: list):
+        self.dataset = dataset
+
 
     def __init_subclass__(cls):
         required_class_variables = ["team_1", "team_2"]
@@ -51,17 +97,21 @@ class BasePattern(metaclass=ABCMeta):
                 )
 
     @abstractmethod
-    def get_teams(self):
+    def get_games(self):
         return NotImplemented
 
 
 class ScoringPattern(BasePattern, metaclass=ABCMeta):
+    str_to_datetime = lambda self, g: datetime.strptime(g['Date'], '%d/%m/%Y')
 
-    team_1 = {}
-    team_2 = {}
+    @abstractmethod
+    def team_1(self) -> dict:
+        pass
 
-    def __init__(self, dataset):
-        self.dataset = dataset
+
+    @abstractmethod
+    def team_2(self) -> dict:
+        pass
 
 
     def get_filter(self, filter_by, table):
@@ -69,6 +119,20 @@ class ScoringPattern(BasePattern, metaclass=ABCMeta):
             get_team_games(self.dataset, t) <= filter_by['max']
 
 
+    def get_games(self, dataset: list = None):
+        """ Detecting games with team_1 and team_2 only. """
+        teams_1, teams_2 = self.get_teams()
+
+        if not dataset: dataset = self.dataset
+        sorted_dateset = sorted(dataset, key=self.str_to_datetime)
+
+        return list(filter(lambda g:
+            (g['HomeTeam'] in teams_2 and g['AwayTeam'] in teams_1) or
+            (g['HomeTeam'] in teams_1 and g['AwayTeam'] in teams_2),
+            sorted_dateset))
+
+
+    @functools.lru_cache(maxsize=None)
     def get_teams(self):
         scoring_table = get_season_table(self.dataset, metric='scored')
         cons_table = get_season_table(self.dataset, metric='conceded')
@@ -101,14 +165,18 @@ class ScoringPattern(BasePattern, metaclass=ABCMeta):
         return teams_1, teams_2
 
 
-class StrongWithWeekPattern(ScoringPattern):
+class StrongWithWeakPattern(ScoringPattern):
 
-    team_1 = {
-        'attack': { 'min': 1.3, 'max': 3.5 },
-        'defence': { 'min': 0.3, 'max': 1.3 }
-    }
+    @property
+    def team_1(self):
+        return {
+            'attack': { 'min': 1.3, 'max': 3.5 },
+            'defence': { 'min': 0.3, 'max': 1.3 }
+        }
 
-    team_2 = {
-        'attack': { 'min': 0.5, 'max': 1.3 },
-        'defence': { 'min': 1.3, 'max': 2.5 }
-    }
+    @property
+    def team_2(self):
+        return {
+            'attack': { 'min': 0.5, 'max': 1.3 },
+            'defence': { 'min': 1.3, 'max': 2.5 }
+        }
--- fox.cub.utils/testing/slave.py
+++ fox.cub.utils/testing/slave.py
@@ -6,13 +6,14 @@ model results with actual results.
 """
 
 from utils import *
-from testing.helpers import TestSessionResult, StrongWithWeekPattern
+from testing.helpers import (
+    TestSessionResult,
+    StrongWithWeakPattern,
+    BasePattern)
 
 class SlaveFoxCubTest:
 
-    str_to_datetime = lambda self, g: datetime.strptime(g['Date'], '%d/%m/%Y')
-
-    def __init__(self, tournament, pattern = None):
+    def __init__(self, tournament, pattern : BasePattern = None):
         # setup http client
         self.fox_cub_client = FoxCub(tournament)
         # search teams by pattern, find games only with this teams
@@ -20,7 +21,7 @@ class SlaveFoxCubTest:
         if pattern:
             self.team_pattern = pattern
         else:
-            self.team_pattern = StrongWithWeekPattern
+            self.team_pattern = StrongWithWeakPattern
 
 
     def get_test_teams_by_points(self, stats_data, team1_pos_min,
@@ -41,35 +42,12 @@ class SlaveFoxCubTest:
             stats_dataset: dataset used to get teams statistics.
                 In some cases test_dataset and stats_dataset may be equal
         """
+        pattern = self.team_pattern(stats_dataset)
+        teams_1, teams_2 = pattern.get_teams()
 
-        results = TestSessionResult()
-        scoring_table = get_season_table(stats_dataset, metric='scored')
-        cons_table = get_season_table(stats_dataset, metric='conceded')
-
-        teams_1, teams_2 = self.team_pattern(stats_dataset).get_teams()
-
-        for t in teams_1:
-            # save team1 stats
-            results.scored_1.append(
-                scoring_table[t] / get_team_games(stats_dataset, t))
-            results.conceded_1.append(
-                cons_table[t] / get_team_games(stats_dataset, t))
-
-        for t in teams_2:
-            # save team2 stats
-            results.scored_2.append(
-                scoring_table[t] / get_team_games(stats_dataset, t))
-            results.conceded_2.append(
-                cons_table[t] / get_team_games(stats_dataset, t))
-
-        skip_games = len(test_dataset) * 0.0
-        sorted_games = sorted(test_dataset, key=self.str_to_datetime)
-        process_games = sorted_games[:]
-
-        games = list(filter(lambda g:
-            (g['HomeTeam'] in teams_2 and g['AwayTeam'] in teams_1) or
-            (g['HomeTeam'] in teams_1 and g['AwayTeam'] in teams_2),
-            process_games))
+        results = TestSessionResult(teams_1, teams_2)
+        results.set_scoring_results(stats_dataset)
+        games = pattern.get_games(test_dataset)
 
         session_id = test_fox_cub(games,
                                   stats_dataset,
@@ -79,35 +57,9 @@ class SlaveFoxCubTest:
         for i, g in enumerate(games):
             results.totals_2_5.append(is_total_under(g, total=2.5))
             results.totals_3_5.append(is_total_under(g, total=3.5))
-            results.btts.append(float(g['FTHG']) > 0 and float(g['FTAG']) > 0)
-
-            self.process_results(results, g,
-                                 self.fox_cub_client.results[session_id][i],
-                                 teams_1)
-            results.model_results.append(
-                self.fox_cub_client.results[session_id][i])
+            results.set_actual_results(g)
+            model_result = self.fox_cub_client.results[session_id][i]
+            results.set_model_results(model_result)
 
         self.fox_cub_client.clear_results(session_id)
         return results
-
-
-    @staticmethod
-    def process_results(results, game, fox_cub_res, teams_1):
-        """ Determine Team1 and Team2 for actual and model results"""
-        if game['HomeTeam'] in teams_1:
-            results.actual_results_team1.\
-                append(float(game['FTHG']) - float(game['FTAG']))
-            results.actual_results_team2.\
-                append(float(game['FTAG']) - float(game['FTHG']))
-        else:
-            results.actual_results_team2.\
-                append(float(game['FTHG']) - float(game['FTAG']))
-            results.actual_results_team1.\
-                append(float(game['FTAG']) - float(game['FTHG']))
-
-        if fox_cub_res['HomeTeam'] in teams_1:
-            fox_cub_res['Team1'] = "Home"
-            fox_cub_res['Team2'] = "Away"
-        else:
-            fox_cub_res['Team1'] = "Away"
-            fox_cub_res['Team2'] = "Home"
